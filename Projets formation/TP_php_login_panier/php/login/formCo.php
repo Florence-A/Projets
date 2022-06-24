0771b117a5143e14287ebf49eb82996a6c1e40c6
Florence-A <89253733+Florence-A@users.noreply.github.com>
2022-06-24 11:52:39 +0200

    24-06

---
+++ Projets formation/TP_php_login_panier/php/login/formCo.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../../css/sp_style.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+
+    <h2>Connexion</h2>
+
+        <form action="co.php" method="POST">
+
+            
+            <input type="email" name="mail" placeholder="Email"><br><br>
+
+            <input type="password" name="mdp" placeholder="Mot de passe"><br><br>
+
+            <input type="submit" value=":)"><br><br><br>
+
+        </form>
+    
+</body>
+</html>
